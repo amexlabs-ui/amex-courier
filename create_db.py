fa5be0b873816449922c4c31afca46d89964bdce
amexlabs-ui <Acefieldcreditunion@aol.com>
2026-03-18 20:13:54 +0100

    Modify database schema and create admin table

--- create_db.py
+++ create_db.py
@@ -3,36 +3,27 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
 
-# USERS
-c.execute("""
-CREATE TABLE IF NOT EXISTS users(
-id INTEGER PRIMARY KEY AUTOINCREMENT,
-username TEXT,
-email TEXT,
-password TEXT,
-role TEXT
-)
-""")
-
-# SHIPMENTS
 c.execute("""
 CREATE TABLE IF NOT EXISTS shipments(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
-tracking TEXT,
+tracking TEXT UNIQUE,
 status TEXT,
-location TEXT,
-lat TEXT,
-lng TEXT
+location TEXT
 )
 """)
 
-# 🔥 CREATE ADMIN USER (IMPORTANT)
-c.execute("SELECT * FROM users WHERE username='admin'")
+# CREATE DEFAULT ADMIN
+c.execute("""
+CREATE TABLE IF NOT EXISTS admin(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+username TEXT,
+password TEXT
+)
+""")
+
+c.execute("SELECT * FROM admin WHERE username='admin'")
 if not c.fetchone():
-    c.execute("""
-    INSERT INTO users(username,email,password,role)
-    VALUES('admin','admin@amex.com','1778','admin')
-    """)
+    c.execute("INSERT INTO admin(username,password) VALUES('admin','1234')")
 
 conn.commit()
 conn.close()
