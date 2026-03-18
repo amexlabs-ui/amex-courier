eaae36377bc7cbe1063246c119dd1346e0a360a7
amexlabs-ui <Acefieldcreditunion@aol.com>
2026-03-18 17:16:31 +0100

    Initialize database with users and shipments tables
    Added tables for users and shipments, and created an admin user if not exists.

--- create_db.py
+++ create_db.py
@@ -3,6 +3,7 @@ import sqlite3
 conn = sqlite3.connect("database.db")
 c = conn.cursor()
 
+# USERS
 c.execute("""
 CREATE TABLE IF NOT EXISTS users(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -13,6 +14,7 @@ role TEXT
 )
 """)
 
+# SHIPMENTS
 c.execute("""
 CREATE TABLE IF NOT EXISTS shipments(
 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,6 +25,14 @@ lat TEXT,
 lng TEXT
 )
 """)
+
+# 🔥 CREATE ADMIN USER (IMPORTANT)
+c.execute("SELECT * FROM users WHERE username='admin'")
+if not c.fetchone():
+    c.execute("""
+    INSERT INTO users(username,email,password,role)
+    VALUES('admin','admin@amex.com','1778','admin')
+    """)
 
 conn.commit()
 conn.close()
